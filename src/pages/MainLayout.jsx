9b3ecd513bc6151be6dea29a583d0cbbcdd1ffa3
piyush-vaviya <email>
2022-09-17 14:39:04 +0530

    feat : remove console of testing

--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -3,8 +3,6 @@ import Sidebar from "../components/Sidebar";
 import Header from "../components/Header";
 
 const MainLayout = (props) => {
-  console.log("hii");
-  console.log("viren");
   return (
     <>
       <Sidebar />
