c645d54ccb053da5de8192e9e05dd8586c6d38e4
piyush-vaviya <email>
2022-10-10 21:19:40 +0530

    chore: faulty code to create merge conflict

--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import ProfileSetup from '../components/ProfileSetup'
 import Sidebar from "../components/Sidebar";
+import { MdOutlineMail } from 'react-icons/md'
+import { BsTelephone } from 'react-icons/bs'
+import ContactItem from '../components/utils/ContactItem'
 import Header from "../components/Header";
+// import Invitation from "../components/utils/Invitation";
+import MessageSender from '../components/MessageSender'
 
 const MainLayout = (props) => {
   return (
